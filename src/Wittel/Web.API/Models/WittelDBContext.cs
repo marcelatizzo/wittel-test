98671dc4ce7689a6e249fa8ffecb5a0eab2595b7
Marcela Tizzo Soares <marcelats.dev@gmail.com>
2019-06-25 04:11:29 -0300

    Criação do DBContext para EF

---
+++ src/Wittel/Web.API/Models/WittelDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web.API.Models
+{
+    public class WittelDBContext : DbContext
+    {
+        public WittelDBContext() : base()
+        { }
+
+        public DbSet<Product> Produtos { get; set; }
+
+        public DbSet<User> Usuarios { get; set; }
+
+        public DbSet<Order> Pedidos { get; set; }
+
+        public DbSet<OrderItem> ItensPedido { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
